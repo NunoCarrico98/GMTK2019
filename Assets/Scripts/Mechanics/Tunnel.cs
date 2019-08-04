196f77f1ed2bcc8ef275d35ff509d6bb4572f747
Nuno Carriço <nuno.carrico.98@gmail.com>
2019-08-04 18:47:37 +0100

    Add new levels (tunnel and space)

---
+++ Assets/Scripts/Mechanics/Tunnel.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Tunnel : MonoBehaviour
+{
+	[SerializeField] private Material[] materials;
+	[SerializeField] private float scaleFactor;
+	[SerializeField] private float rotationAmount;
+
+	[Header("After Complete Tunnel")]
+	[SerializeField] private float desiredScale;
+	[SerializeField] private float initialScaleDuration;
+	[SerializeField] private float fastScaleDuration;
+
+	private TunnelManager tunnelManager;
+	private bool doOnce = false;
+
+	public int MatIndex { get; set; } = 1;
+	public int OrderInLayer { get; set; } = 1;
+	public int SpawnedSquares { get; set; } = 1;
+	public bool CanClick { get; set; } = true;
+
+	private void Awake()
+	{
+		tunnelManager = FindObjectOfType<TunnelManager>();
+	}
+
+	private void Update()
+	{
+		SpawnSquare();
+	}
+
+	private void SpawnSquare()
+	{
+		if (Input.GetMouseButtonDown(0) &&
+			CanClick &&
+			SpawnedSquares != tunnelManager.MaxSquares)
+		{
+			CanClick = false;
+
+			GameObject squareInst = Instantiate(gameObject, transform.position, transform.rotation);
+			squareInst.transform.localScale = transform.localScale;
+			squareInst.GetComponent<SpriteRenderer>().material = materials[MatIndex];
+			squareInst.GetComponent<SpriteRenderer>().sortingOrder = OrderInLayer;
+			squareInst.transform.DOScale(transform.localScale.x * scaleFactor, .2f);
+			Vector3 newRot = new Vector3(0, 0, squareInst.transform.eulerAngles.z + rotationAmount);
+			squareInst.transform.DORotate(newRot, .2f);
+
+			UpdateMaterialIndex();
+			OrderInLayer++;
+			SpawnedSquares++;
+
+			squareInst.GetComponent<Tunnel>().MatIndex = MatIndex;
+			squareInst.GetComponent<Tunnel>().OrderInLayer = OrderInLayer;
+			squareInst.GetComponent<Tunnel>().SpawnedSquares = SpawnedSquares;
+
+			tunnelManager.AddToList(squareInst);
+		}
+	}
+
+	private void UpdateMaterialIndex() => MatIndex = MatIndex == 1 ? 0 : 1;
+}
