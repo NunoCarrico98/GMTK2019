38f951b50dbadd153f90606774fb9bcdfd6ebf66
Nuno Carriço <nuno.carrico.98@gmail.com>
2019-08-04 21:10:59 +0100

    Fix tunnel

--- Assets/Scripts/TunnelManager.cs
+++ Assets/Scripts/TunnelManager.cs
@@ -7,9 +7,9 @@ public class TunnelManager : MonoBehaviour
 	[SerializeField] private int maxSquares;
 
 	[Header("After Complete Tunnel")]
+	[SerializeField] private float waitTime;
 	[SerializeField] private float desiredSize;
 	[SerializeField] private float decreaseValue;
-	[SerializeField] private float fasterDecrease;
 
 	private Camera[] cams;
 	private List<GameObject> squares;
@@ -24,7 +24,7 @@ public class TunnelManager : MonoBehaviour
 
 	private void Update()
 	{
-		FinishLevel();
+		StartCoroutine(FinishLevel());
 	}
 
 	public void AddToList(GameObject square)
@@ -37,24 +37,16 @@ public class TunnelManager : MonoBehaviour
 		return squares[squares.Count - 1];
 	}
 
-	public void FinishLevel()
+	public IEnumerator FinishLevel()
 	{
 		if (squares.Count >= MaxSquares - 1)
 		{
+			yield return new WaitForSeconds(waitTime);
 			foreach (Camera c in cams)
 			{
 				c.orthographicSize -= decreaseValue;
 				if (c.orthographicSize <= desiredSize) c.orthographicSize = desiredSize;
 			}
-		}
-	}
-
-	private IEnumerator IncreaseScaleDuration()
-	{
-		while (decreaseValue > fasterDecrease)
-		{
-			yield return null;
-			decreaseValue -= Time.deltaTime;
 		}
 	}
 }
