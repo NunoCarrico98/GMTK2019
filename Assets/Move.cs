3fbd3dfb0af428a932c415d9c8f34ee3cdf4395b
Nuno Carriço <nuno.carrico.98@gmail.com>
2019-08-04 20:58:03 +0100

    Fix fall sound

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -7,14 +7,10 @@ public class Move : MonoBehaviour
 {
 	[SerializeField] private float moveDuration;
 	[SerializeField] private float speed;
+	[SerializeField] private AudioSource source;
+	[SerializeField] private AudioClip carIdle;
 
 	private bool move = true;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-		//transform.DOLocalMoveY(0, moveDuration);
-    }
 
 	private void Update()
 	{
@@ -31,7 +27,18 @@ public class Move : MonoBehaviour
 				move = false;
 				transform.position = new Vector3(0,0,0);
 				transform.DOPunchPosition(new Vector3(0,1,0), 0.3f, 10, 0f);
+				source.Play();
+				StartCoroutine(PlayCarIdle());
 			}
 		}
+	}
+
+	private IEnumerator PlayCarIdle()
+	{
+		yield return new WaitForSeconds(1f);
+
+		source.clip = carIdle;
+		source.loop = true;
+		source.Play();
 	}
 }
