1808f1ce770217dc975089457ecd26f78eac777b
Nuno Carriço <nuno.carrico.98@gmail.com>
2019-08-04 20:45:42 +0100

    Fix car scene fall

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -6,10 +6,32 @@ using DG.Tweening;
 public class Move : MonoBehaviour
 {
 	[SerializeField] private float moveDuration;
+	[SerializeField] private float speed;
+
+	private bool move = true;
 
     // Start is called before the first frame update
     void Start()
     {
-		transform.DOLocalMoveY(0, moveDuration);
+		//transform.DOLocalMoveY(0, moveDuration);
     }
+
+	private void Update()
+	{
+		MoveSquare();
+	}
+
+	private void MoveSquare()
+	{
+		if (move)
+		{
+			transform.Translate(new Vector3(0, -1, 0) * speed * Time.deltaTime);
+			if (transform.position.y <= 0)
+			{
+				move = false;
+				transform.position = new Vector3(0,0,0);
+				transform.DOPunchPosition(new Vector3(0,1,0), 0.3f, 10, 0f);
+			}
+		}
+	}
 }
